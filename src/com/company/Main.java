8530ffb4141edb42310eb6bd231aea10818df027
Chanly Septian <37769960+chanlyseptian@users.noreply.github.com>
2018-03-29 08:31:30 +0700

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input : ");
+        int n = in.nextInt();
+        long fib[] = new long[n];
+
+        fib[0] = 1;
+        fib[1] = 1;
+
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+
+        }
+        System.out.println("Output : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+
+
+    }
+}
